dee4f9fdf49515b6aeb4e8890bf632a3c8f8127f
ryantantiern <ryantantiern@gmail.com>
2016-12-20 11:21:17 +0800

    first commit with laravel/passport set up

---
+++ database/migrations/2016_12_20_024735_create_shared_routes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSharedRoutesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shared_routes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('route_id');
+            $table->integer('sender_id');
+            $table->integer('receiver_id');
+            $table->boolean('accepted')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('shared_routes');
+    }
+}
