0a95b78729c4eef5c49fe00d855873208075ee01
Stefan Manole <zcabman@ucl.ac.uk>
2017-01-31 21:26:02 +0200

    Refactored the app + added Create Routes page

--- app/www/scripts/controllers.js
+++ app/www/scripts/controllers.js
@@ -1,54 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-.config(function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider
-    .state('login', {
-      url: "/login",
-      templateUrl: "templates/login.html",
-      controller: "LoginCtrl"
-    })
-    .state('register', {
-      url: "/register",
-      templateUrl: "templates/register.html",
-      controller: "RegisterCtrl"
-    })
-    .state('dashboard', {
-          url: "/dashboard",
-          templateUrl: "templates/dashboard.html",
-          controller: "DashboardCtrl"
-    })
-    .state('friends', {
-              url: "/friends",
-              templateUrl: "templates/friends.html",
-              controller: "FriendsCtrl"
-     })
-
-
-  $urlRouterProvider.otherwise("/login");
-})
+angular.module('starter.controllers',[])
 
 .controller('LoginCtrl', function($scope, $state, loginData) {
    $scope.data = {};
@@ -90,8 +40,11 @@ angular.module('starter', ['ionic'])
 
 .controller('DashboardCtrl', function($scope, $state, loginData) {
   $scope.user_data = loginData.getForm();
-  $scope.viewFriends = function(){
+  $scope.goViewFriends = function(){
       $state.go('friends');
+  };
+  $scope.goCreateRoute = function(){
+      $state.go('create_route');
   };
 })
 
@@ -105,19 +58,13 @@ angular.module('starter', ['ionic'])
   {
     $scope.friends.push({'name': $scope.rawFriends[i].firstName+' '+$scope.rawFriends[i].lastName});
   }
-  $scope.goToDashboard = function(){
+  $scope.goBack = function(){
       $state.go('dashboard');
         }
 })
 
-.service('loginData', function() {
- return {
-   form: {},
-   getForm: function() {
-     return this.form;
-   },
-   updateForm: function(form) {
-     this.form = form;
-   }
- }
+.controller('CreateRouteCtrl', function($scope, $state) {
+  $scope.goBack = function(){
+      $state.go('dashboard');
+  };
 })
